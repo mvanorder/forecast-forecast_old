1dd6f61db7b59b2ec4b1f91fc891191e09c04f9a
Flying Dutchman <brian.vanhoff@gmail.com>
2020-04-15 07:57:11 -0400

    moved and deleted sort logs and trans & load files

--- export.js
+++
@@ -1,8 +0,0 @@
-
-print("id,name,startDate")
-cursor = db.code.find();
-while (cursor.hasNext()) {
-    jsonObject = cursor.next();
-    print(jsonObject._id.valueOf() + "," + jsonObject.current + ",\"" + jsonObject.five_day +"\"")
-
-}
