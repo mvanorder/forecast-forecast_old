768285f2472cd55430f62dfb54add97a13316488
Flying Dutchman <brian.vanhoff@gmail.com>
2020-04-18 19:38:25 -0400

    working get_and_make.py module

--- ETL/Extract/make_instants.py
+++ ETL/Extract/make_instants.py
@@ -178,25 +178,23 @@ def delete_command_for(data):
     ''' 
     from pymongo import DeleteOne
 
-    # if "Weather" in data:
-    #     filters = {'zipcode': data['Weather'].pop('zipcode'), 'instant': data['Weather'].pop('instant')}
-    #     updates = {'$set': {'weather': data['Weather']}}
-    ### this if for the processing of data in OWM.forecasted and OWM.observed.
+    # catch the error if this is processing data entered by another module or version of this one, but otherwise expect
+    # there to be 
     if "Weather" in data:
         try:
+            filters = {'zipcode': data['Weather'].pop('zipcode'), 'instant': data['Weather'].pop('instant')}
+            updates = {'$set': {'weather': data['Weather']}}
+        except KeyError:
+            # this if for the processing of data in OWM.forecasted and OWM.observed.
             data['Weather']['time_to_instant'] =  data['Weather'].pop('reference_time') - data['reception_time']
             filters = {'zipcode': data.pop('zipcode'), 'instant': data.pop('instant')}
             updates = {'$set': {'weather': data['Weather']}}
         except KeyError:
             print('caught KeyError')
-    # else:
-    #     filters = {'zipcode': data.pop('zipcode'), 'instant': data.pop('instant')}
-    #     updates = {'$push': {'forecasts': data}} # append the forecast object to the forecasts list
-    ### Same thing down here ###
     else:
         try:
             filters = {'zipcode': data.pop('zipcode'), 'instant': data.pop('instant')}
-            updates = {'$push': {'forecasts': data}} 
+            updates = {'$push': {'forecasts': data}} # append the forecast object to the forecasts list
         except KeyError:
             print('caught keyerror')
     return UpdateOne(filters, updates,  upsert=True)
@@ -211,24 +209,14 @@ def make_load_list_from_cursor(pymongoCursorOnWeather):
     '''
 
     update_list = []
-#     try:
-#         pymongoCursorOnWeather.next()
-#     except StopIteration:
-#         print('got to the end of the cursor')
-        
     # check the first entry to know whether it's forecast or observation
     # print(pymongoCursorOnWeather.count_documents())
-    
     if 'Weather' in pymongoCursorOnWeather[0]:
-#         print('it is Weather')
         for obj in pymongoCursorOnWeather:
             update_list.append(update_command_for(obj))
         return update_list
     else:
-#         print('it is something other than Weather; going into for loop')
-        
         for obj in pymongoCursorOnWeather:
-
             # Trying things that will capture any of the formats published over the developemnet period.
             try:
                 if 'reception_time':
@@ -256,30 +244,7 @@ def make_load_list_from_cursor(pymongoCursorOnWeather):
                 print(f'printing to {filename}')
                 with open(filename, 'a') as f:
                     f.write(obj['_id']+'\n')
-                # print(obj['_id'])
-#             print('completed loop iteration')
-#         print(f'here is the update list {update_list}')
         return update_list
-
-# def convert_keys_to_string(dictionary):
-#     """Recursively converts dictionary keys to strings."""
-#     if not isinstance(dictionary, dict):
-#         return dictionary
-#     return dict((str(k), convert_keys_to_string(v)) 
-#         for k, v in dictionary.items())
-
-# def convert(data):
-#     if isinstance(data, basestring):
-#         return str(data)
-#     elif isinstance(data, int):
-#         return str(data)
-#     elif isinstance(data, collections.Mapping):
-#         return dict(map(convert, data.iteritems()))
-#     elif isinstance(data, collections.Iterable):
-#         return type(data)(map(convert, data))
-#     else:
-#         return data
-
 
 def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
     ''' move or copy a collection within and between databases 
@@ -293,193 +258,36 @@ def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
     :param filters: a filter for the documents to be copied from the collection. By default all collection docs will be copied
     :type filters: dict
     '''
+
     client = Client(host=host, port=port)
-#     print(original[0])
     copy = []
     n=0
-#     if type(col) == dict:
-#         for key, value in col:
-#             copy.append(str(value))
-#     elif type(col) == list:
-#         for value in col:
-#             copy.append(str(value))
-#     else:
-#         original = col.find(filters)
-#         for item in col.find(filters):
-#             copy.append(item)
     original = col.find(filters)
     for item in col.find(filters):
         copy.append(item)
-    print(f'type(copy) = {type(copy)}')
-#     print(f'copy[0] = {copy[0]}')
-    print(f'len(copy) = {len(copy)}. Just about to name the dbncol and insert_many into it')
     destination = dbncol(client, collection=destination_col, database=destination_db)
     inserted_ids = destination.insert_many(copy).inserted_ids
-    print(type(inserted_ids))
-#     if type(inserted_ids) == dict:
-#         print('inserted_ids is a dict and I am converting the keys to strings...')
-#         convert_keys_to_string(inserted_ids)
-    print('Insertion complete: the inserted id\'s are: {inserted_ids}') # list of the doc ids that were successfully inserted
     if delete == True:
-        # remove all the documents from the origin collection
+        # remove all the documents from the original collection
         for row in inserted_ids:
             filters = {'_id': row}
             col.delete_one(filters)
-        print(f'MOVED docs from {col} to {destination}, that is {destination_db}.{destination_col}')
-    else:
-        print(f'COPIED docs in {col} to {destination}, that is {destination_db}.{destination_col}')
 
-# def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
-#     ''' move or copy a collection within and between databases 
-    
-#     :param col: the collection to be copied
-#     :type col: a pymongo collection or 
-#     :param destination_col: the collection you want the documents copied into
-#     :type destination_col: a pymongo.collection.Collection object
-#     :param destination_db: the database with the collection you want the documents copied into
-#     :type destination_db: a pymongo database pymongo.databse.Database
-#     :param filters: a filter for the documents to be copied from the collection. By default all collection docs will be copied
-#     :type filters: dict
-#     '''
-#     client = Client(host=host, port=port)
-# #     print(original[0])
-#     copy = []
-#     n=0
-# #     if type(col) == dict:
-# #         for key, value in col:
-# #             copy.append(str(value))
-# #     elif type(col) == list:
-# #         for value in col:
-# #             copy.append(str(value))
-# #     else:
-# #         original = col.find(filters)
-# #         for item in col.find(filters):
-# #             copy.append(item)
-#     original = col.find(filters)
-#     for item in col.find(filters):
-#         copy.append(item['_id'])
-#     print(f'type(copy) = {type(copy)}')
-#     print(f'len(copy) = {len(copy)}. Just about name the dbncol and insert_many into it')
-#     destination = dbncol(client, collection=destination_col, database=destination_db)
-#     inserted_ids = destination.insert_many(copy).inserted_ids
-#     print(type(inserted_ids))
-# #     if type(inserted_ids) == dict:
-# #         print('inserted_ids is a dict and I am converting the keys to strings...')
-# #         convert_keys_to_string(inserted_ids)
-#     print(f'Insertion complete: the inserted id\'s are: {inserted_ids}') # list of the doc ids that were successfully inserted
-#     if delete == True:
-#         # remove all the documents from the origin collection
-#         for row in inserted_ids:
-#             filters = {'_id': value}
-#             col.delete_one(filters)
-#         print(f'MOVED docs from {col} to {destination}, that is {destination_db}.{destination_col}')
-#     else:
-#         print(f'COPIED docs in {col} to {destination}, that is {destination_db}.{destination_col}')
+def make_instants(client):
+    ''' make the instant documents, as many as you can, with the data in the named database'''
 
-def make_instants():
-    client = Client(host=host, port=port)
-    # set the database and collection to pull from
     database = "test"
     cast_col = dbncol(client, "cast_temp", database=database)
     obs_col = dbncol(client, "obs_temp", database=database)
     inst_col = dbncol(client, "instant_temp", database=database)
-    num_docs_cast = cast_col.count_documents({}) # get a count of the number of documents under the cursor so that you
-    num_docs_obs = obs_col.count_documents({})   # can know when to break out of the while loop so not to cause error
-    print(num_docs_obs, num_docs_cast)
     forecasts = cast_col.find({})
     observations = obs_col.find({})
     inst_col.create_index([('instant', pymongo.DESCENDING)])
     inst_col.bulk_write(make_load_list_from_cursor(forecasts))
     inst_col.bulk_write(make_load_list_from_cursor(observations))
-            
-    print('attempting to move those docs just sorted....I am expecting that this is every document in the cast_temp collection')
+
     copy_docs(cast_col, database, 'cast_archive', delete=True)
-    print('attempting to move those docs just sorted....I am assuming that this is every dcoument in the obs_temp collection')
     copy_docs(obs_col, database, 'obs_archive', delete=True)
 
-
-# __name__ = '__main__'
-# if __name__ == "__main__":
-    
-#     client = Client(host=host, port=port)
-#     # set the database and collection to pull from
-#     database = "test"
-#     cast_col = dbncol(client, "cast_temp", database=database)
-#     obs_col = dbncol(client, "obs_temp", database=database)
-#     inst_col = dbncol(client, "instant_temp", database=database)
-#     num_docs_cast = cast_col.count_documents({}) # get a count of the number of documents under the cursor so that you
-#     num_docs_obs = obs_col.count_documents({})   # can know when to break out of the while loop so not to cause error
-#     print(num_docs_obs, num_docs_cast)
-#     forecasts = cast_col.find({})
-#     observations = obs_col.find({})
-#     inst_col.create_index([('instant', pymongo.DESCENDING)])
-#     inst_col.bulk_write(make_load_list_from_cursor(forecasts))
-#     inst_col.bulk_write(make_load_list_from_cursor(observations))
-            
-#     print('attempting to move those docs just sorted....I am expecting that this is every document in the cast_temp collection')
-#     copy_docs(cast_col, database, 'cast_archive', delete=True)
-#     print('attempting to move those docs just sorted....I am assuming that this is every dcoument in the obs_temp collection')
-#     copy_docs(obs_col, database, 'obs_archive', delete=True)
-
-# __name__ = '__main__'
-# if __name__ == "__main__":
-    
-#     client = Client(host=host, port=port)
-#     # set the database and collection to pull from
-#     database = "test"
-#     cast_col = dbncol(client, "cast_temp", database=database)
-#     obs_col = dbncol(client, "obs_temp", database=database)
-#     inst_col = dbncol(client, "instant_temp", database=database)
-#     num_docs_cast = cast_col.count_documents({}) # get a count of the number of documents under the cursor so that you
-#     num_docs_obs = obs_col.count_documents({})   # can know when to break out of the while loop so not to cause error
-#     print(num_docs_obs, num_docs_cast)
-#     m, n, i = 0, 0, 0 # n to count the total number of documents sorted, i to track the number of passes of the loop
-#     while m < num_docs_cast and n < num_docs_obs:
-#         print(f'in the while loop with i={i}')
-#         # this block will get 1000 forecast documents, create the bulk_write commands list, and execute the
-#         # bulk write function.
-#         forecasts = cast_col.find({})[m:m+1000]
-#         m += 1000
-#         observations = obs_col.find({})[n:n+1000]
-#         n += 1000
-#         inst_col.create_index([('instant', pymongo.DESCENDING)])
-# #         print('OBSERVATIONS.................\n\n', '\n', observations[0])
-        
-#         # within each line: create the update command for each ducument update, create the list of those updates,
-#         # execute the bulk_write command on the list of updates, and finally set a dict of inserted object id's.
-#         bwr_casts = inst_col.bulk_write(make_load_list_from_cursor(forecasts)).upserted_ids
-#         bwr_obs = inst_col.bulk_write(make_load_list_from_cursor(observations)).upserted_ids
-# #         bwr_casts = convert_key_to_string(bwr_casts)
-# #         bwr_obs = convert_key_to_string(bwr_obs)
-# #         print(bwr_casts)
-#         print('attempting to move those docs just sorted....bwr_casts= {bwr_casts}\n')
-#         copy_docs(cast_col, database, 'cast_archive', bwr_casts, delete=True)
-#         print('attempting to move those docs just sorted....bwr_obs= {bwr_obs}\n')
-#         copy_docs(obs_col, database, 'obs_archive', bwr_obs, delete=True)
-        
-#         i += 1
-#         num_docs_cast = cast_col.count_documents({}) # get a count of the number of documents under the cursor so that you
-#         print(f'num_docs_cast= {num_docs_cast}')
-#         num_docs_obs = obs_col.count_documents({})   # can know when to break out of the while loop so not to cause error
-#         print(f'num_docs_obs= {num_docs_obs}')
-#         print(f'sorted {n} forecast arrays after {i} main while loop iterations')
-    
-#     print('past while loop')
-    
-#     # repeat the commands from the while loop, this time on the last <1000 documents from the collection
-#     forecasts = cast_col.find({})[n:]
-#     observations = obs_col.find({})[n:]
-#     inst_col.create_index([('instant', pymongo.DESCENDING)])
-#     # within each line: create the update command for each ducument update, create the list of those updates,
-#     # execute the bulk_write command on the list of updates, and finally set a dict of inserted object id's.
-#     bwr_casts = inst_col.bulk_write(make_load_list_from_cursor(forecasts)).upserted_ids
-#     m += len(bwr_casts)
-#     print(f'just sorted the last {len(bwr_casts) } documents from the "forecasted" collection')
-#     print(bwr_casts)
-#     print('starting sort job of the "observed" collection')
-#     bwr_obs = inst_col.bulk_write(make_load_list_from_cursor(observations)).upserted_ids
-#     n += len(bwr_obs)
-#     print(f'just sorted the last {len(bwr_obs)} documents from the "observed" collection')
-# #     print('starting the copy and delete the sorted ')
-# #     copy_docs(cast_col, database, 'cast_archive', bwr_casts, delete=True)
-# #     copy_docs(cast_col, database, 'obs_archive', bwr_obs, delete=True)
+if __name__ == "__main__":
+    client = Client(host=host, port=port)
