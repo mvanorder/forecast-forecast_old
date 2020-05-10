377617d5dc9534a494d14473baee3c803a71fc24
Flying Dutchman <brian.vanhoff@gmail.com>
2020-05-10 08:44:27 -0400

    added dict key print function

--- overalls.py
+++ overalls.py
@@ -8,3 +8,16 @@ def read_list_from_file(filename):
     """
     with open(filename, "r") as z_list:
         return z_list.read().strip().split(',')
+    
+def key_list(d=dict):
+    ''' Write the dict keys to a list and return that list 
+    
+    :param d: A python dictionary
+    :return: A list fo the keys from the python dictionary
+    '''
+    
+    keys = d.keys()
+    key_list = []
+    for k in keys:
+        key_list.append(k)
+    return key_list
