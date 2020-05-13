c665018e9b241c677ab233731e634a591d020176
Flying Dutchman <brian.vanhoff@gmail.com>
2020-05-12 20:34:54 -0400

    update cron

---
+++ cron/db_ops.py
@@ -0,0 +1,133 @@
+''' Useful functions for forecast-forecast specific operations '''
+
+import time
+
+from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection, ReturnDocument
+from pymongo.errors import ConnectionFailure, InvalidDocument, DuplicateKeyError, OperationFailure, ConfigurationError
+from urllib.parse import quote
+
+# from config import user, password, socket_path, host, port
+database = 'test'
+
+def Client(host=None, port=None, uri=None):
+    ''' Create and return a pymongo MongoClient object. Connect with the given parameters if possible, switch to local if the
+    remote connection is not possible, using the default host and port.
+    
+    :param host: the local host to be used. defaults within to localhost
+    :type host: sting
+    :param port: the local port to be used. defaults within to 27017
+    :type port: int
+    :param uri: the remote server URI. must be uri encoded
+    type uri: uri encoded sting'''
+    
+    if host and port:
+        try:
+            client = MongoClient(host=host, port=port)
+            return client
+        except ConnectionFailure:
+            # connect to the remote server if a valid uri is given
+            if uri:
+                print('caught ConnectionFailure on local server. Trying to make it with remote')
+                client = MongoClient(uri)
+                print(f'established remote MongoClient on URI={uri}')
+                return client
+            print('caught ConnectionFailure on local server. Returning None')
+            return None
+    elif uri:
+        # verify that the connection with the remote server is active and switch to the local server if it's not
+        try:
+            client = MongoClient(uri)
+            return client
+        except ConfigurationError:
+            print(f'Caught configurationError in client() for URI={uri}. It was likely triggered by a DNS timeout.')
+            client = MongoClient(host=host, port=port)
+            print('connection made with local server, even though you asked for the remote server')
+            return client
+
+def dbncol(client, collection, database=database):
+    ''' Make a connection to the database and collection given in the arguments.
+
+    :param client: a MongoClient instance
+    :type client: pymongo.MongoClient
+    :param database: the name of the database to be used. It must be a database name present at the client
+    :type database: str
+    :param collection: the database collection to be used.  It must be a collection name present in the database
+    :type collection: str
+    
+    :return col: the collection to be used
+    :type: pymongo.collection.Collection
+    '''
+
+    db = Database(client, database)
+    col = Collection(db, collection)
+    return col
+
+def load(data, client, database, collection):
+    ''' Load data to specified database collection. Also checks for a preexisting document with the same instant and 
+    zipcode, and updates it in the case that there was already one there.
+
+    :param data: the dictionary created from the api calls
+    :type data: dict
+    :param client: a MongoClient instance
+    :type client: pymongo.MongoClient
+    :param database: the database to be used
+    :type database: str
+    :param collection: the database collection to be used
+    :type collection: str
+    '''
+
+    col = dbncol(client, collection, database=database)
+
+    # set the appropriate database collections, filters and update types
+    if collection == 'instant':
+        filters = {'zipcode':data['zipcode'], 'instant':data['instant']}
+        updates = {'$push': {'forecasts': data}} # append the forecast object to the forecasts list
+        try:
+            # check to see if there is a document that fits the parameters. If there is, update it, if there isn't, upsert it
+            return col.find_one_and_update(filters, updates,  upsert=True)
+        except DuplicateKeyError:
+            return(f'DuplicateKeyError, could not insert data into {collection}.')
+    elif collection == 'observed' or collection == 'forecasted':
+        try:
+            col.insert_one(data)
+            return
+        except DuplicateKeyError:
+            return(f'DuplicateKeyError, could not insert data into {collection}.')
+    else:
+        try:
+            filters = {'zipcode':data['zipcode'], 'instant':data['instant']}
+            updates = {'$set': {'forecasts': data}} # append the forecast object to the forecasts list
+            return col.find_one_and_update(filters, updates,  upsert=True)
+        except DuplicateKeyError:
+            return(f'DuplicateKeyError, could not insert data into {collection}.')
+
+def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
+    ''' move or copy a collection within and between databases 
+    
+    :param col: the collection to be copied
+    :type col: a pymongo collection
+    :param destination_col: the collection you want the documents copied into
+    :type destination_col: a pymongo.collection.Collection object
+    :param destination_db: the database with the collection you want the documents copied into
+    :type destination_db: a pymongo database pymongo.databse.Database
+    :param filters: a filter for the documents to be copied from the collection. By default all collection docs will be copied
+    :type filters: dict
+    '''
+    client = Client(host=host, port=port)
+    original = col.find(filters).batch_size(1000)
+    copy = []
+    for item in original:
+        copy.append(item)
+    destination = dbncol(client, collection=destination_col, database=destination_db)
+    inserted_ids = destination.insert_many(copy).inserted_ids # list of the doc ids that were successfully inserted
+    if delete == True:
+        # remove all the documents from the origin collection
+        for item in inserted_ids:
+            filter = {'_id': item}
+            col.delete_one(filter)
+        print(f'MOVED docs from {col} to {destination}, that is {destination_db}.{destination_col}')
+    else:
+        print(f'COPIED docs in {col} to {destination}, that is {destination_db}.{destination_col}')
+
---
+++ cron/instant.py
@@ -0,0 +1,180 @@
+''' Defines the Instant class and some useful functions. '''
+
+
+class Instant:
+
+    def __init__(self, _id, forecasts=[], observations={}):
+        
+        self._id = _id
+        self.casts = forecasts
+        self.obs = observations
+        self.as_dict = {'_id': self._id,
+                        'forecasts': self.casts,
+                        'observations': self.obs
+                        }
+    
+    @property
+    def count(self):
+        ''' Get the count of the elements in self.casts. '''
+        
+        return len(self.casts)
+    
+    @property
+    def itslegit(self):
+        ''' Check the instant's weathers array's count and if it is 40, then the
+        document is returned.
+
+        :param instant: the instant docuemnt to be legitimized
+        :type instant: dictionary
+        '''
+        
+        self.count()
+        if self.count == 40:
+            return True
+        else:
+            return False
+    
+    def to_dbncol(self, collection='test'):
+        ''' Load the data to the database. 
+
+        :param collection: the collection name
+        :type collection: string
+        '''
+
+        from config import database
+        from db_ops import dbncol
+
+        col = dbncol(client, collection, database=database)
+        col.update_one({'_id': self._id}, {'$set': self.as_dict}, upsert=True)
+
+    
+def cast_count_all(instants):
+    ''' get a tally for the forecast counts per document 
+
+    :param instants: docmuments loaded from the db.instants collection ### NOT
+    Instant class objects
+    :type instants: list
+    '''
+    
+    n = 0
+    collection_cast_counts = {}
+
+    # Go through each doc in the collection and count the number of items in
+    # the forecasts array. Add to the tally for that count.
+    for doc in instants:
+        n = len(doc['forecasts'])
+        # Move the legit instants to the permenant database
+        if n >= 40:
+            load_legit(doc)
+        if n in collection_cast_counts:
+            collection_cast_counts[n] += 1
+        else:
+            collection_cast_counts[n] = 1
+    return collection_cast_counts
+
+
+def sweep(instants):
+    ''' Move any instant that has a ref_time less than the current next
+    ref_time and with self.count less than 40. This is getting rid of the
+    instnats that are not and will never be legit. 
+
+    :param instants: a list of Instant objects
+    '''
+    
+    import time
+    
+    import pymongo
+    from pymongo.cursor import Cursor
+    from Extract.make_instants import find_data
+    from config import client, database
+    from db_ops import dbncol
+    
+    col = dbncol(client, 'instant_temp', database=database)
+    n = 0
+    # Check the instant type- it could be a dict if it came from the database,
+    # or it could be a list if it's a bunch of instant objects, or a pymongo
+    # cursor over the database.
+    if type(instants) == dict:
+        for key, doc in instants:
+            if key['instant'] < time.time()-453000:  # 453000sec: about 5 days
+                col.delete_one(doc)
+                n += 1
+    elif type(instants) == list:
+        for doc in instants:
+            if doc['instant'] < time.time()-453000:
+                col.delete_one(doc)
+                n += 1
+    elif type(instants) == pymongo.cursor.Cursor:
+        for doc in instants:
+            if doc['instant'] < time.time()-453000:
+                col.delete_one(doc)
+                n += 1
+    else:
+        print(f'You want me to sweep instants that are {type(instants)}\'s.')
+    return
+
+def find_legit(instants):
+    ### THIS DOES NOT WORK ###
+    ''' find the 'legit' instants within the list
+
+     :param instants: all the instants pulled from the database
+     :type instants: list
+     :return: list of instants with a complete forecasts array
+     '''
+
+    i = [item for item in instants if len(item['forecasts']) >= 40]
+    return f'legit list is {len(i)} items long'
+    ### maybe you should make the instant documents pulled form the database
+    ### represented as Instants in memory. Then you could use the Instant
+    ### methods you've been writing.
+
+
+def load_legit(legit_list):
+    ''' Load the 'legit' instants to the remote database and delete from temp.
+    This process does not delete the 
+
+    :param collection: the collection you want to pull instants from
+    :type collection: pymongo.collection.Collection
+    '''
+
+    from pymongo.errors import DuplicateKeyError
+    from config import client, database
+    from db_ops import dbncol
+    from db_ops import copy_docs
+    
+### this should load to legit_inst in owmap for production ###
+#         col = dbncol(client, 'legit_inst', 'owmap')
+    col = dbncol(client, 'legit_inst', database=database)
+    try:
+        col.insert_one(legit_list)
+    except DuplicateKeyError:
+        col = dbncol(client, 'instant_temp', database=database)
+        col.delete_one(legit_list)
+        ### saved for later, when doing it on bulk ###
+#     col.insert_many(legit_list)
+    # Now go to the temp_instants collection and delete the instants just
+    # loaded to legit_inst.
+#     col.delete_many(legit_list)
+    return
+
+
+import time
+start_time = time.time() # This is to get the total runtime if this script is
+                         # run as __main__
+if __name__ == '__main__':
+    ''' Connect to the database, then move all the legit instants to the remote
+    database and clear out any instants that are past and not legit.
+    '''
+    
+    import config
+    import db_ops
+
+    # Set the database here if you must, but it's better to do that in the
+    # config.py file
+#     database = 'owmap'
+    collection = 'instant_temp'
+    col = db_ops.dbncol(config.client, collection, database=config.database)
+    cast_count_all(col.find({}))
+    sweep(col.find({}))
+
+    print(f'Total op time for instant.py was {time.time()-start_time} seconds')
